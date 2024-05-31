bc2dde2851ab86eeb4f516e8a16a2b5d95931449
jay vekariya <vekariya.ja@northeastern.edu>
2024-05-30 22:54:17 -0400

    feat: jenkins infra tf
    created vpc, sg, sg rules, and ec2 instance with terraform

---
+++ variable.tf
@@ -0,0 +1,79 @@
+variable "region" {
+  type = string
+}
+
+variable "vpc" {
+  type = object({
+    cidr_block = string
+    tags       = map(string)
+  })
+}
+
+variable "subnet" {
+  type = object({
+    cidr_block = string
+    tags       = map(string)
+  })
+}
+
+variable "igw" {
+  type = object({
+    tags = map(string)
+  })
+}
+
+variable "rt" {
+  type = object({
+    tags = map(string)
+  })
+}
+
+variable "route" {
+  type = object({
+    destination_cidr_block = string
+  })
+}
+
+variable "sg" {
+  type = object({
+    name = string
+    tags = map(string)
+  })
+}
+
+variable "sg_rules" {
+  type = map(object({
+    type      = string
+    from_port = number
+    to_port   = number
+    protocol  = string
+    cidr_blocks = list(string)
+  }))
+}
+
+variable "ami_data" {
+  type = object({
+    owners      = list(string)
+    most_recent = bool
+    filters = list(object({
+      name   = string
+      values = list(string)
+    }))
+  })
+}
+
+variable "ec2_instance" {
+  type = object({
+    instance_type = string
+    tags          = map(string)
+  })
+}
+
+variable "data_eip" {
+  type = object({
+    filters = list(object({
+      name   = string
+      values = list(string)
+    }))
+  })
+}
